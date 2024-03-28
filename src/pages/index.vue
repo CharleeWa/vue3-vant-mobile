caa1ff302299aba29b6fd49ce185d6dfc2521956
sven <hellosc@qq.com>
2024-03-28 22:17:11 +0800

    feat: Support vue i18n

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import useAppStore from '@/stores/modules/app'
+import { languageColumns, locale } from '@/utils/i18n'
 
 definePage({
   name: 'main',
@@ -24,22 +25,50 @@ function toggle() {
   appStore.swithMode(isDark.value ? 'dark' : 'light')
 }
 
-const menuItems = [
-  { title: '💿 Mock 指南', route: 'mock' },
-  { title: '📊 Echarts 演示', route: 'charts' },
-  { title: '🎨 Unocss 示例', route: 'unocss' },
-  { title: '🍍 持久化 Pinia 状态', route: 'counter' },
-  { title: '🙅 404页 演示', route: 'unknown' },
-]
+const { t } = useI18n()
+
+const languageValues = computed({
+  get() {
+    return [locale.value]
+  },
+  set(values: Array<string>) {
+    locale.value = values[0]
+  },
+})
+
+const menuItems = computed(() => ([
+  { title: t('home.mockGuide'), route: 'mock' },
+  { title: t('home.echartsDemo'), route: 'charts' },
+  { title: t('home.unocssExample'), route: 'unocss' },
+  { title: t('home.persistPiniaState'), route: 'counter' },
+  { title: t('home.404Demo'), route: 'unknown' },
+]))
+
+const showLanguagePicker = ref(false)
 </script>
 
 <template>
   <VanCellGroup inset>
-    <VanCell center title="🌗 暗黑模式">
+    <VanCell center :title="$t('home.darkMode')">
       <template #right-icon>
         <VanSwitch v-model="checked" size="20px" aria-label="on/off Dark Mode" @click="toggle()" />
       </template>
     </VanCell>
+    <van-field
+      is-link
+      readonly
+      name="picker"
+      :label="$t('home.language')"
+      @click="showLanguagePicker = true"
+    />
+    <van-popup v-model:show="showLanguagePicker" position="bottom">
+      <van-picker
+        v-model="languageValues"
+        :columns="languageColumns"
+        @confirm="showLanguagePicker = false"
+        @cancel="showLanguagePicker = false"
+      />
+    </van-popup>
 
     <template v-for="item in menuItems" :key="item.route">
       <VanCell :title="item.title" :to="item.route" is-link />
